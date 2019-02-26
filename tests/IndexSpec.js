a4b27e81effd5bcb1ac69db03583d3c4ba251849
Denis Rostokin <denis.rostokin@osinit.com>
2019-02-26 22:42:42 +0300

    Added README.md

--- tests/IndexSpec.js
+++ tests/IndexSpec.js
@@ -1,7 +1,23 @@
+import jsx, { Component } from 'custom-elements-jsx'
+
 describe('single test', () => {
     it('runs correctly', () => {
-        const a = 'hello'
+        class CustomElement extends Component {
+            render() {
+                return <div>Hello</div>
+            }
+        }
 
-        expect(a).toBe('hello')
+        window.customElements.define('custom-element', CustomElement)
+
+        const element = <custom-element />
+
+        document.body.appendChild(element)
+
+        expect(element.outerHTML).toBe(
+            '<custom-element><div>Hello</div></custom-element>'
+        )
+
+        document.body.removeChild(element)
     })
 })
